8bee80552df4e04d44beac868a7db682d34569c4
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-02-09 16:36:18 +0100

    position buttons

---
+++ style/components/Cards.stories.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Components/Card",
+};
+
+export const Error = () => /* html */ `
+button {
+    position: relative;
+    top: 200px;
+    left: 500px;
+    margin: 20px;
+  }
+`;
