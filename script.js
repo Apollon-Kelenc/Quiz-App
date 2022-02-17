e30531d8ec423df140f810423faa1f8a2736bc9b
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-02-17 17:57:23 +0100

    added show answer function to all buttons

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const showAnswer = document.querySelector('[data-js="answer-button"]');
 const firstAnswer = document.querySelector('[data-js="first-answer"]');
 
+const showAnswer2 = document.querySelector('[data-js="answer-button2"]');
+const secondAnswer = document.querySelector('[data-js="second-answer"]');
+
+const showAnswer3 = document.querySelector('[data-js="answer-button3"]');
+const thirdAnswer = document.querySelector('[data-js="third-answer"]');
+
+const showAnswer4 = document.querySelector('[data-js="answer-button4"]');
+const fourthAnswer = document.querySelector('[data-js="fourth-answer"]');
+
 showAnswer?.addEventListener('click', () => {
   firstAnswer.classList.toggle('display-toggle');
+});
+
+showAnswer2?.addEventListener('click', () => {
+  secondAnswer.classList.toggle('display-toggle');
+});
+
+showAnswer3?.addEventListener('click', () => {
+  thirdAnswer.classList.toggle('display-toggle');
+});
+
+showAnswer4?.addEventListener('click', () => {
+  fourthAnswer.classList.toggle('display-toggle');
 });
 
 const textInput = document.querySelector('[data-js="textfieldInput"]');
